a92673d3f82fa5dfbff954c42c7fedc677ed6974
Greg-Olson <olson.gregory714@gmail.com>
2023-12-15 18:25:00 -0700

    Added collision detection

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,19 @@ mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
-    pub use legion::world::SubWorld;
     pub use legion::*;
+    pub use legion::world::SubWorld;
+    pub use legion::systems::CommandBuffer;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-    pub use crate::camera::*;
     pub use crate::components::*;
-    pub use crate::map::*;
-    pub use crate::map_builder::*;
     pub use crate::spawner::*;
+    pub use crate::map::*;
     pub use crate::systems::*;
+    pub use crate::map_builder::*;
+    pub use crate::camera::*;
 }
 
 use prelude::*;
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,13 +1,17 @@
 use crate::prelude::*;
+
 mod map_render;
 mod player_input;
 mod entity_render;
-
+mod collisions;
 
 pub fn build_scheduler() -> Schedule {
+    //START: collision_schedule
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .add_system(collisions::collisions_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
+    //END: collision_schedule
 }
