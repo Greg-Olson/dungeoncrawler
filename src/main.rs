ad3dc5ae0c450b887e9f1d82a46d055133708b24
Greg-Olson <olson.gregory714@gmail.com>
2023-12-18 11:15:53 -0800

    Added turn based movement and a messaging intent system

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![warn(clippy::pedantic)]
 
-//START: prelude
-mod camera;
 mod components;
+mod spawner;
 mod map;
 mod map_builder;
-mod spawner;
 mod systems;
+mod camera;
+mod turn_state;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -23,43 +23,46 @@ mod prelude {
     pub use crate::systems::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
+    pub use crate::turn_state::*;
 }
 
 use prelude::*;
-//END: prelude
 
 //START: state
 struct State {
-    ecs: World,
+    ecs : World,
     resources: Resources,
-    systems: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule
 }
+//END: state
 
 impl State {
-    //START: statenew
-    //START: statenew
     fn new() -> Self {
         let mut ecs = World::default();
         let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
-        map_builder
-            .rooms
+        map_builder.rooms
             .iter()
             .skip(1)
             .map(|r| r.center())
             .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
-
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        //START: statenew
+        resources.insert(TurnState::AwaitingInput);
         Self {
             ecs,
             resources,
-            systems: build_scheduler(),
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler()
         }
+        //END: statenew
     }
-    //END: statenew
 }
 
 impl GameState for State {
@@ -69,22 +72,35 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        //START: dispatch
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_state {
+            TurnState::AwaitingInput => self.input_systems.execute(
+                &mut self.ecs, 
+                &mut self.resources
+            ),
+            TurnState::PlayerTurn => {
+                self.player_systems.execute(&mut self.ecs, &mut self.resources);
+            }
+            TurnState::MonsterTurn => {
+                self.monster_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+        }
         render_draw_buffer(ctx).expect("Render error");
+        //END: dispatch
     }
 }
-//END: state
 
 fn main() -> BError {
     let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // <callout id="co.dungeongfx.dimensions" />
-        .with_tile_dimensions(32, 32) // <callout id="co.dungeongfx.tiledimensions" />
-        .with_resource_path("resources/") // <callout id="co.dungeongfx.resources" />
-        .with_font("dungeonfont.png", 32, 32) // <callout id="co.dungeongfx.font" />
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // <callout id="co.dungeongfx.con1" />
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // <callout id="co.dungeongfx.con2" />
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
