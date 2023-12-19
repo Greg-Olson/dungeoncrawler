da333c4fee9d5601927cd860b5e9531153fe1fde
Greg-Olson <olson.gregory714@gmail.com>
2023-12-18 16:45:47 -0800

    Added visibility and map memory

--- src/systems/chasing.rs
+++ src/systems/chasing.rs
@@ -1,72 +1,75 @@
-//START: boilerplate
 use crate::prelude::*;
 
+//START: header
 #[system]
 #[read_component(Point)]
 #[read_component(ChasingPlayer)]
+#[read_component(FieldOfView)]
 #[read_component(Health)]
 #[read_component(Player)]
 pub fn chasing(
-    #[resource] map: &Map,
-    ecs: &SubWorld,
-    commands: &mut CommandBuffer
+  #[resource] map: &Map,
+  ecs: &SubWorld,
+  commands: &mut CommandBuffer
 ) {
-    let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
-    let mut positions = <(Entity, &Point, &Health)>::query();
-    let mut player = <(&Point, &Player)>::query();
-    //END: boilerplate
-    //START: findplayer
-    let player_pos = player.iter(ecs).nth(0).unwrap().0;
-    let player_idx = map_idx(player_pos.x, player_pos.y);
-    //END: findplayer
+  let mut movers= <(Entity, &Point, &ChasingPlayer, &FieldOfView)>::query();
+  let mut positions = <(Entity, &Point, &Health)>::query();
+  let mut player = <(&Point, &Player)>::query();
+  //END: header
+  let player_pos = player.iter(ecs).nth(0).unwrap().0;
+  let player_idx = map_idx(player_pos.x, player_pos.y);
 
-    // START: dijkstra
-    let search_targets = vec![player_idx];// <callout id="co.gauntlet.searchvec" />
-    let dijkstra_map = DijkstraMap::new(
-        SCREEN_WIDTH,// <callout id="co.gauntlet.dims" />
-        SCREEN_HEIGHT,
-        &search_targets,
-        map,// <callout id="co.gauntlet.mapref" />
-        1024.0// <callout id="co.gauntlet.maxiters" />
-    );
-    //END: dijkstra
+  let search_targets = vec![player_idx];
+  let dijkstra_map = DijkstraMap::new(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    &search_targets,
+    map,
+    1024.0
+  );
 
-    // START: chase
-    movers.iter(ecs).for_each(| (entity, pos, _) | {// <callout id="co.gauntlet.eachmover" />
-        let idx = map_idx(pos.x, pos.y);
-        if let Some(destination) = DijkstraMap::find_lowest_exit(
-            &dijkstra_map, idx, map
-        )
-        {// <callout id="co.gauntlet.findlowest" />
-            let distance = DistanceAlg::Pythagoras.distance2d(*pos, *player_pos);// <callout id="co.gauntlet.distance_to_player" />
-            let destination = if distance > 1.2 {// <callout id="co.gauntlet.ifgr12" />
-                map.index_to_point2d(destination)
-            } else {
-                *player_pos
-            };
-    //END: chase
+  //START: viewcheck
+  movers.iter(ecs).for_each(| (entity, pos, _, fov) | {
+    if !fov.visible_tiles.contains(&player_pos) {
+      return;
+    }
+    //END: viewcheck
+    let idx = map_idx(pos.x, pos.y);
+    if let Some(destination) = DijkstraMap::find_lowest_exit(
+      &dijkstra_map, idx, map)
+    {
+      let distance = DistanceAlg::Pythagoras.distance2d(
+        *pos, *player_pos);
+      let destination = if distance > 1.2 {
+        map.index_to_point2d(destination)
+      } else {
+        *player_pos
+      };
 
-    //START: repeat
-            let mut attacked = false;
-            positions
-                .iter(ecs)
-                .filter(|(_, target_pos, _)| **target_pos == destination)
-                .for_each(|(target, _, _)| {
-                    if ecs.entry_ref(*target).unwrap().get_component::<Player>()
-                        .is_ok() {
-                            commands
-                                .push(((), WantsToAttack{ 
-                                    attacker: *entity,
-                                    target: *target
-                                }));
-                    }
-                    attacked = true;
-                });
+      let mut attacked = false;
+      positions
+        .iter(ecs)
+        .filter(|(_, target_pos, _)| **target_pos == destination)
+        .for_each(|(target, _, _)| {
+          if ecs.entry_ref(*target)
+            .unwrap()
+            .get_component::<Player>().is_ok() 
+          {
+            commands
+              .push(((), WantsToAttack{ 
+                attacker: *entity,
+                target: *target
+              }));
+          }
+          attacked = true;
+        });
 
-            if !attacked {
-                commands
-                    .push(((), WantsToMove{ entity: *entity, destination }));
-            }
-        }
-    });//END: repeat
+      if !attacked {
+        commands
+          .push(((), WantsToMove{ 
+            entity: *entity, destination 
+          }));
+      }
+    }
+  });
 }
--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -1,22 +1,39 @@
 use crate::prelude::*;
 
+//START: er_header
 #[system]
 #[read_component(Point)]
 #[read_component(Render)]
-pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
-    let mut draw_batch = DrawBatch::new(); 
+#[read_component(FieldOfView)]
+#[read_component(Player)]
+pub fn entity_render(
+    #[resource] camera: &Camera,
+    ecs: &SubWorld,
+) {
+    let mut renderables = <(&Point, &Render)>::query();
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    //END: er_header
+    let mut draw_batch = DrawBatch::new();
     draw_batch.target(1);
     let offset = Point::new(camera.left_x, camera.top_y);
 
-    <(&Point, &Render)>::query()
-        .iter(ecs)
+    //START: er_filter
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+
+    renderables.
+        iter(ecs)
+        // START_HIGHLIGHT
+        .filter(|(pos, _)| player_fov.visible_tiles.contains(&pos))
+        // END_HIGHLIGHT
         .for_each(|(pos, render)| {
             draw_batch.set(
                 *pos - offset,
                 render.color,
-                 render.glyph
+                render.glyph
             );
         }
     );
-    draw_batch.submit(5000).expect("Batch error"); 
+    //END: er_filter
+
+    draw_batch.submit(5000).expect("Batch error");
 }
